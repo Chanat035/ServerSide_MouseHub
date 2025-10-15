bdb91b96efd5ef14f87dc9385061c44ac89b9972
manonz11 <66070166@kmitl.ac.th>
2025-10-16 02:15:33 +0700

    productpage

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -6,16 +6,22 @@ const useWebRoute = (router) => {
     res.render("index", { title: "หน้าแรก MouseHub", user: res.locals.user });
   });
 
+  // หน้า products (public)
+  router.get("/products", (req, res) => {
+    res.render("products", { title: "สินค้า MouseHub", user: res.locals.user });
+  });
+
   // หน้า admin
   router.get("/admin", authMiddleware("admin", true), (req, res) => {
     res.render("admin", { title: "แผงควบคุมผู้ดูแลระบบ", user: res.locals.user });
   });
 
-  // หน้า settings / account
+  // หน้า settings / account (ต้อง login)
   router.get("/settings", authMiddleware(undefined, true), (req, res) => {
     const show = req.query.show || "change";
     res.render("account", { user: res.locals.user, show });
   });
 };
+
 
 export default useWebRoute;
